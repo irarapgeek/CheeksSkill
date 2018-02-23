3fbdc6d07287c64ac67d6e72e64d2234ba31a71c
The Genius <rapgeek@gmail.com>
2018-02-22 18:14:58 -0600

    Initial Solution Checkin

---
+++ Controllers/AlexaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlexaSkill.Controllers
+{
+    [Route("api/alexa")]
+    public class AlexaController : Controller
+    {
+        [HttpPost]
+        public dynamic Genie(dynamic request)
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttribbutes = new {},
+                response = new 
+                {
+                    outputSpeech = new 
+                    {
+                        type = "PlanText",
+                        text = "Hello World"
+                    },
+                    card = new 
+                    {
+                        text = "Simple",
+                        title = "Genie",
+                        content = "Hello\nCruel World!"
+                    },
+                    shouldEndSession = true
+                },
+            };
+        }
+    }
+}
